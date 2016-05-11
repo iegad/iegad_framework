bace74b2ed0172816f778610b9864c9d55df4e45
iegad <iegad@vip.qq.com>
2016-05-11 19:30:30 +0800

    update 2016-05-11

--- libiegad/src/thrift_ex/thrift_ex_macro.h
+++ libiegad/src/thrift_ex/thrift_ex_macro.h
@@ -39,41 +39,43 @@ typedef std::function<##__delegate_sign_##> __event_type_;
 
 
 // 宏实现定义
-#define __XXX_IfFactory__(a)							a##IfFactory
-#define __XXX_If__(a)									a##If
-#define __XXX_Handler__(a)							a##Handler
-#define __XXX_ProcessorFactory__(a)			a##ProcessorFactory
+#define __XXX_IfFactory__(a)				a##IfFactory
+#define __XXX_If__(a)					a##If
+#define __XXX_Handler__(a)				a##Handler
+#define __XXX_ProcessorFactory__(a)		a##ProcessorFactory
 #define __XXX_ProcessorCloneFactory__(a)	a##ProcessorCloneFactory
-#define __XXX_ProtocolFactory__(a)				::apache::thrift::protocol::T##a##ProtocolFactory
-#define __XXX_Client__(a)								a##Client
-#define __XXX_Protocol__(a)							::apache::thrift::protocol::T##a##Protocol
-#define __XXX_Class_Member(a, b)				a##::##b
+#define __XXX_ProtocolFactory__(a)			::apache::thrift::protocol::T##a##ProtocolFactory
+#define __XXX_Client__(a)					a##Client
+#define __XXX_Protocol__(a)				::apache::thrift::protocol::T##a##Protocol
+#define __XXX_Class_Member(a, b)			a##::##b
 
 
 // 宏调用声明定义
-#define XXX_IfFactory(a)							__XXX_IfFactory__(a)
-#define XXX_If(a)										__XXX_If__(a)
-#define XXX_Handler(a)								__XXX_Handler__(a)
-#define XXX_ProcessorFactory(a)				__XXX_ProcessorFactory__(a)	
+#define XXX_IfFactory(a)					__XXX_IfFactory__(a)
+#define XXX_If(a)						__XXX_If__(a)
+#define XXX_Handler(a)					__XXX_Handler__(a)
+#define XXX_ProcessorFactory(a)			__XXX_ProcessorFactory__(a)	
 #define XXX_ProcessorCloneFactory(a)		__XXX_ProcessorCloneFactory__(a)	
-#define XXX_ProtocolFactory(a)				__XXX_ProtocolFactory__(a)
-#define XXX_Client(a)								__XXX_Client__(a)
-#define XXX_Protocol(a	)							__XXX_Protocol__(a)
-#define XXX_Class_Member(a, b)				__XXX_Class_Member(a, b)
+#define XXX_ProtocolFactory(a)			__XXX_ProtocolFactory__(a)
+#define XXX_Client(a)					__XXX_Client__(a)
+#define XXX_Protocol(a	)				__XXX_Protocol__(a)
+#define XXX_Class_Member(a, b)			__XXX_Class_Member(a, b)
 
 
 // 协议定义
-#define BINARY_PROTOCOL Binary
-#define COMPACT_PROTOCOL Compact
-#define JSON_PROTOCOL JSON
+#define BINARY_PROTOCOL		Binary
+#define COMPACT_PROTOCOL	Compact
+#define JSON_PROTOCOL		JSON
 
 
 // 服务端模型定义
 typedef	 ::apache::thrift::server::TThreadedServer		THRIFT_THREADED_SERVER;
 typedef ::apache::thrift::server::TThreadPoolServer		THRIFT_THREADPOOL_SERVER;
-typedef ::apache::thrift::server::TNonblockingServer	THRIFT_NON_BLOCKING_SERVER;
+typedef ::apache::thrift::server::TNonblockingServer		THRIFT_NON_BLOCKING_SERVER;
 
-#define _APP_TIME_OUT (1000 * 60) // 1分钟超时值
+
+// 1分钟超时值
+#define _APP_TIME_OUT (1000 * 60) 
 
 
 
